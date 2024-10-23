403f2d84b94d4c021b23fdd94748b634ae8354b9
Seshagiri Nuthalapati <106400388+seshu-kf@users.noreply.github.com>
2024-10-23 12:35:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ var pg = require("pg");
 var conString = "pg://kfmerge_admin:UateMistuTcH@usedpgsqlpayadb01.postgres.database.azure.com:5432/clientdataintake?sslmode=prefer";
 //var conString = 'pg://kfone_core_user:Ad%2FNJ1GGgtoH_@kfone-dev-use1-shared-psql-001.cnosksgsira5.us-east-1.rds.amazonaws.com:5432/kfone?schema=kfone_core?sslmode=prefer';
 var client = new pg.Client(conString);
-client.connect();
+await client.connect();
+
+const result = await client.query('SELECT NOW()')
+console.log(result)
+ 
+await client.end()
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
